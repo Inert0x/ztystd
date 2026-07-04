e144d5f9cbfbb15b3543c7178e14a97728c7ef25
Inert0x <freshmentolegend@hotmail.com>
2026-07-04 13:31:23 +0800

    I miscommitted an empty file, and it's now solved.

---
+++ ztyc++.h
@@ -0,0 +1,21 @@
+// Begin DataStructure
+#include "DataStructure/SegmentTree.hpp"
+#include "DataStructure/Treap.hpp"
+
+// Begin Graph
+#include "Graph/Graph.hpp"
+#include "Graph/GraphTools.hpp"
+#include "Graph/ShortestPath.hpp"
+
+// Begin Math
+#include "Math/Matrix.hpp"
+#include "Math/Moding.hpp"
+#include "Math/Prime.hpp"
+
+// Begin String
+#include "String/StringMatching.hpp"
+#include "String/Trie.hpp"
+
+// Begin utilities
+#include "Debugger.hpp"
+#include "FastIO.hpp"
